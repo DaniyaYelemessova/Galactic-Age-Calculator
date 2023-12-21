d30efb1554c958830cb66d1dc5e49857bc4e15a0
Daniya Yelemessova <daniya@collingsmail.com>
2023-12-21 18:40:55 -0500

    Add Red Phase, bad fail test, for venus age calculation

--- __tests__/age.test.js
+++ __tests__/age.test.js
@@ -10,4 +10,9 @@ describe('AgeCalculator', () => {
     const ageCalculation = new AgeCalculator(27);
     expect(ageCalculation.mercuryAgeCalculation()).toEqual(112);
   });
+
+  test('should correctly calculate Venus age', () => {
+    const ageCalculation = new AgeCalculator(56);
+    expect(ageCalculation.venusAgeCalculation()).toEqual(90);
+  });
 });
